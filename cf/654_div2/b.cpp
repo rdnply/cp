38d210efee746f7946a4b8a9f0479e305aba1c06
rdnply <polyanscky.rodion@yandex.ru>
2021-01-26 23:10:51 +0300

    add #654 div2 some tasks

---
+++ cf/654_div2/b.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define pb push_back
+#define pf push_front
+#define PI acos(-1)
+#define EPS 1e-5
+#define sz(v) ((int)(v).size())
+#define all(v) v.begin(),v.end()
+#define forn(i, n) for(ll i=0;i<(ll)n;i++)
+#define fi first
+#define se second
+ 
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		ll n, r;
+		cin >> n >> r;
+		if(n > r) {
+			cout << (1+r)*r/2 << endl;
+		} else {
+			cout << (1+n-1)*(n-1)/2+1 << endl;
+		}
+	}
+	return 0;
+}
+
