d56feec37427017755812c877f1a8e9fafc283f6
rdnply <polyanscky.rodion@yandex.ru>
2021-01-27 12:51:13 +0300

    add two tasks to #654 div2

---
+++ cf/654_div2/d.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define IOS ios_base::sync_with_stdio(false); cin.tie(0);
+#define pb push_back
+#define pf push_front
+#define PI acos(-1)
+#define EPS 1e-5
+#define sz(v) ((int)(v).size())
+#define all(v) v.begin(),v.end()
+#define forn(i, n) for(ll i=0;i<(ll)n;i++)
+#define fi first
+#define se second
+ 
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int n, k;
+		cin >> n >> k;
+		vector<vector<int>> res(n, vector<int>(n));
+		int p = 0, q = 0;
+		cout << (k%n==0? 0: 2) << endl;
+		while(k--) {
+			res[p][q] = 1;
+			p++, q++, q %= n;
+			if(p == n) {
+				p = 0, q++, q %= n;
+			}
+		}
+		for(auto i: res) {
+			for(auto j: i) {
+				cout << j;
+			}
+			cout << endl;
+		}
+	}
+	return 0;
+}
+
